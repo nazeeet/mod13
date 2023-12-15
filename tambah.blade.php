20a50eeb32da554b7089041c6b5716db82ca21ef
nazeeet <150373948+nazeeet@users.noreply.github.com>
2023-12-15 22:21:14 +0700

    Create tambah.blade.php

---
+++ tambah.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Pegawai</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="text-center">Tambah Pegawai</h1>
+        <form action="{{ url('/pegawai/store') }}" method="POST" class="row g-3">
+            @csrf
+            <div>
+                <label for="name" class="form-label">Nama:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Masukkan nama pegawai" required>
+            </div>
+            <div>
+                <label for="posisi" class="form-label">Posisi:</label>
+                <input type="text" class="form-control" id="posisi" name="posisi" placeholder="Masukkan posisi pegawai" required>
+            </div>
+            <div>
+                <label for="gaji" class="form-label">Gaji:</label>
+                <input type="number" class="form-control" id="gaji" name="gaji" placeholder="Masukkan gaji pegawai" required>
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary">Tambah Pegawai</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
